7c60d0d7985a2e6c664a85cf411cf2ce0063108c
Aksh Sharma <aksht098@gmail.com>
2026-04-12 12:59:21 +0530

    Enhance age calculation to include months and days

--- DOB_proj.py
+++ DOB_proj.py
@@ -1,9 +1,12 @@
 import streamlit as st
 from datetime import date, datetime
+from dateutil.relativedelta import relativedelta
+
 
 st.title("Calculating users age")
 user_date = st.date_input("Enter your date of birth", value=datetime.now(), min_value = date(1980, 1,1), max_value=datetime.now())
 
-current_year = datetime.now().year
-age = current_year - user_date.year
-st.success(f"Your current age is {age}")
+current_date = datetime.now()
+main = relativedelta(current_date, user_date)
+st.success(f"Your current age is {main.years} year {main.months} months {main.days} days")
+
